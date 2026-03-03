52808d0dba66aea222a7d92593a499da05cff2a9
gui <gui151208@gmail.com>
2026-03-03 16:45:25 -0300

    .

--- src/main/java/com/example/handlingformsubmission/HandlingFormSubmissionApplication.java
+++ src/main/java/com/example/handlingformsubmission/HandlingFormSubmissionApplication.java
@@ -1,4 +1,4 @@
-package com.example.handling_form_submission;
+package com.example.handlingformsubmission;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
